953c718dfe698c8a1deef75e60860f3a0684eff8
HilolaRustamova <mohlaroy86@gmail.com>
2025-07-30 12:18:36 +0100

    added function to generate and append all episode cards to the page

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 function setup() {
   const allEpisodes = getAllEpisodes(); //Get all episodes from episode.js
    makePageForEpisodes(allEpisodes);    //Generate the episode cards.
-}
+};
 // This function helps formate episode code as S02E04
 function formatEpisodeCode(season, number) {
   const seasonStr = season.toString().padStart(2, "0");
   const numberStr = number.toString().padStart(2, "0");
   return `S${seasonStr}E${numberStr}`;
-}
+};
   // Create a card for each episode from the template.
 function createEpisodeCard(episode) {
 const template = document.getElementById("episode-card"); //Get the template element.
@@ -23,7 +23,20 @@ card.querySelector("img").alt = episode.name;
 card.querySelector(".summary").innerHTML = episode.summary;
 return card; //return the filled card.
 };
+//Function that generates and appends all episode cards to the page.
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  rootElem.textContent=`${episodeList.length}Episodes`; //Show count at top
 
+const container = document.getElementById("container");
+container.innerHTML=""; //Clear any existing content.
+
+// Use map to create an array of cards
+const episodeCards = episodeList.map(createEpisodeCard);
+
+// Append all episode cards to the container
+episodeCards.forEach((card) => container.appendChild(card));
+};
 
 
 
